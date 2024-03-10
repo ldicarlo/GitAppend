b16425b484ff90b565ff003ec80f31c73e8d7f60
Luca Di Carlo <luca@dicarlo.email>
2024-03-10 17:53:51 +0100

    wip

--- src/git.rs
+++ src/git.rs
@@ -19,9 +19,6 @@ pub fn commit(repo: &Repository, sign: &Signature) {
         .map_err(|_| git2::Error::from_str("Couldn't find commit"))
         .unwrap();
     let mut index = repo.index().unwrap();
-    // index
-    //     .add_all(["."], git2::IndexAddOption::DEFAULT, None)
-    //     .unwrap();
     let oid = index.write_tree().unwrap();
     let tree = repo.find_tree(oid).unwrap();
     repo.commit(
@@ -36,15 +33,22 @@ pub fn commit(repo: &Repository, sign: &Signature) {
 }
 
 pub fn fetch(repo: &Repository) {
-    repo.fetchhead_foreach(|_, _, _, _| true).unwrap()
+    repo.fetchhead_foreach(|name, _, _, _| {
+        println!("{}", name);
+        true
+    })
+    .unwrap()
 }
 
 pub fn signature() -> Signature<'static> {
     Signature::now("Git-Append", "git-append@git").unwrap()
 }
 
-pub fn status() {}
-
 pub fn add(repo: &Repository, path: String) {
     repo.index().unwrap().add_path(&Path::new(&path)).unwrap();
+}
+
+pub fn push(repo: &Repository) {
+    // repo.remot
+    // repo.remote(name, url).unwrap().push(refspecs, opts)
 }
