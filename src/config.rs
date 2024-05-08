7a7925e39a1b51fe9649875c8672868a35e40a1d
Luca Di Carlo <luca@dicarlo.email>
2024-05-08 15:36:44 +0200

    wip

--- src/config.rs
+++ src/config.rs
@@ -6,7 +6,14 @@ use serde::{Deserialize, Serialize};
 #[serde(deny_unknown_fields)]
 pub struct Config {
     pub settings: String,
-    pub appenders: HashMap<String, HashMap<String, Appender>>,
+    pub appenders: HashMap<String, GitAppender>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone, Eq, PartialEq)]
+#[serde(deny_unknown_fields)]
+pub struct GitAppender {
+    pub git_config: Option<GitConfig>,
+    pub links: HashMap<String, Appender>,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone, Eq, PartialEq)]
@@ -16,4 +23,11 @@ pub struct Appender {
     // pub Option<sorted>: bool
     // pub Option<unique>: bool
     pub password_file: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone, Eq, PartialEq)]
+#[serde(deny_unknown_fields)]
+pub struct GitConfig {
+    pub username: String,
+    pub token_file: String,
 }
